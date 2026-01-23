0e2aa53aa49a1e4d78890a9bb362569b962e9cb9
joaooln <joaooln@gmail.com>
2026-01-23 12:37:19 -0500

    feat: add docker compose

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ api.add_resource(User, '/user')
 api.add_resource(Users, '/users', '/users/<string:cpf>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
