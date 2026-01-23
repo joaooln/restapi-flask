c99a7e7d98fcc43e43e7642c10ae9b972b83ad27
joaooln <joaooln@gmail.com>
2026-01-23 14:41:57 -0500

    fix: connection resused from mongo

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
+
+app.config['MONGODB_SETTINGS'] = {
+    "db": "mydb",
+    "host": "mongo",
+    "port": 27017,
+    "username": "root",
+    "password": "root",
+    "authentication_source": "admin"
+}
+
+
 api = Api(app)
+db = MongoEngine(app)
+
+
+class UserModel(db.Document):
+    cpf = db.StringField(required=True, unique=True)
+    email = db.EmailField(required=True)
+    first_name = db.StringField(required=True)
+    last_name = db.StringField(required=True)
+    birth_date = db.DateField()
 
 
 class Users(Resource):
